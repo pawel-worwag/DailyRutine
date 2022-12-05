fef05e4e598e8b7358670a77827cabddcecac474
Paweł Worwąg <pawel.worwag@gmail.com>
2022-12-05 19:49:58 +0100

    Persistance layer dependency injection

--- src/DailyRutine.Api/Program.cs
+++ src/DailyRutine.Api/Program.cs
@@ -1,9 +1,13 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using DailyRutine.Persistance;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddPersistenceLayer(builder.Configuration);
 
 var app = builder.Build();
 
---
+++ src/DailyRutine.Persistance/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using DailyRutine.Application;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DailyRutine.Persistance
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPersistenceLayer(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<DailyRutineDbContext>(options => options.UseNpgsql(configuration.GetConnectionString("DailyRutineDb")));
+            services.AddScoped<IDailyRutineDbContext, DailyRutineDbContext>();
+            return services;
+        }
+    }
+}
+
