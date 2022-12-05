5c9e463d68fd9ec15074025a700dfc887a507fd7
Paweł Worwąg <pawel.worwag@gmail.com>
2022-12-05 20:33:07 +0100

    MediatR, application layer bdependency injection.

--- src/DailyRutine.Api/Program.cs
+++ src/DailyRutine.Api/Program.cs
@@ -1,4 +1,5 @@
-﻿using DailyRutine.Persistance;
+﻿using DailyRutine.Application;
+using DailyRutine.Persistance;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +9,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddPersistenceLayer(builder.Configuration);
+builder.Services.AddApplicationLayer();
 
 var app = builder.Build();
 
---
+++ src/DailyRutine.Application/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using MediatR;
+using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DailyRutine.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplicationLayer(this IServiceCollection services)
+        {
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+            return services;
+        }
+    }
+}
+
