84c057a13cbbee00eb80f3443bcd85ad63685f4b
Paweł Worwąg <pawel.worwag@gmail.com>
2022-12-05 20:42:42 +0100

    Users DbSet

--- src/DailyRutine.Application/IDailyRutineDbContext.cs
+++ src/DailyRutine.Application/IDailyRutineDbContext.cs
@@ -1,8 +1,12 @@
 ﻿using System;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 namespace DailyRutine.Application
 {
     public interface IDailyRutineDbContext
     {
+        public DbSet<IdentityUser> Users { get; set; }
         Task<int> SaveChangesAsync(CancellationToken cancellationToken);
     }
 }
