294133d28d63fb6b9247e6f974cd530981e6b0fc
Paweł Worwąg <pawel.worwag@gmail.com>
2022-12-06 22:12:08 +0100

    Base global exception middleware

---
+++ src/DailyRutine.Api/GlobalExceptionMiddleware.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net;
+
+namespace DailyRutine.Api
+{
+    public class GlobalExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger _logger;
+
+        public GlobalExceptionMiddleware(RequestDelegate next, ILogger<GlobalExceptionMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch(Exception ex)
+            {
+                _logger.LogError($"[{context.TraceIdentifier}] {ex.Message}");
+                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                await context.Response.WriteAsJsonAsync($"Error message: {ex.Message} \n CorrelationId: {context.TraceIdentifier}");
+            }
+        }
+    }
+
+    public static class GlobalExceptionMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseGlobalExceptionMiddleware(
+            this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<GlobalExceptionMiddleware>();
+        }
+    }
+}
+
--- src/DailyRutine.Api/Program.cs
+++ src/DailyRutine.Api/Program.cs
@@ -1,7 +1,9 @@
-﻿using DailyRutine.Application;
+﻿using DailyRutine.Api;
+using DailyRutine.Application;
 using DailyRutine.Persistance;
 
 var builder = WebApplication.CreateBuilder(args);
+
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -14,6 +16,8 @@ builder.Services.AddApplicationLayer();
 builder.Services.AddControllers();
 
 var app = builder.Build();
+
+app.UseGlobalExceptionMiddleware();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
