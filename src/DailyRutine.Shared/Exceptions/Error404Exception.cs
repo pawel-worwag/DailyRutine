f7063e237c8f94a34df031ca2c6d11773c1cebfe
Paweł Worwąg <pawel.worwag@gmail.com>
2022-12-08 19:33:31 +0100

    404 exception class

---
+++ src/DailyRutine.Shared/Exceptions/Error404Exception.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace DailyRutine.Shared.Exceptions
+{
+    public class Error404Exception : Exception
+    {
+        public Error404Exception()
+        {
+        }
+
+        public Error404Exception(string? message) : base(message)
+        {
+        }
+
+        public Error404Exception(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+        protected Error404Exception(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
+
