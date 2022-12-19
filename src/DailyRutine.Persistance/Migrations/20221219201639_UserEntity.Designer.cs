6873fa0ee2809c1353446f7a86a0b639ee162067
Paweł Worwąg <pawel.worwag@gmail.com>
2022-12-19 21:17:38 +0100

    User entity migration

--- src/DailyRutine.Persistance/Migrations/20221219201639_UserEntity.Designer.cs
+++ src/DailyRutine.Persistance/Migrations/20221219201639_UserEntity.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using DailyRutine.Persistance;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace DailyRutine.Persistance.Migrations
 {
     [DbContext(typeof(DailyRutineDbContext))]
-    partial class DailyRutineDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221219201639_UserEntity")]
+    partial class UserEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/DailyRutine.Persistance/Migrations/20221219201639_UserEntity.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DailyRutine.Persistance.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TimeZoneId",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TimeZoneId",
+                table: "AspNetUsers");
+        }
+    }
+}
