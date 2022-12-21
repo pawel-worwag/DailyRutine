ce20761ddcc716ece9b1f8aa2b7dc364de2c874c
Paweł Worwąg <pawel.worwag@gmail.com>
2022-12-21 20:53:32 +0100

    Base Token Auth

---
+++ src/DailyRutine.Api/Controllers/MeApiController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DailyRutine.Api.Controllers
+{
+    [Route("/api/v1/me")]
+    public class MeApiController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetProfile()
+        {
+            return Ok($"Authenticated: {HttpContext.User.Identity.IsAuthenticated}; Name: {HttpContext.User.Identity.IsAuthenticated}");
+        }
+    }
+}
+
--- src/DailyRutine.Api/Program.cs
+++ src/DailyRutine.Api/Program.cs
@@ -1,8 +1,12 @@
-﻿using DailyRutine.Api;
+﻿using System.Text;
+using DailyRutine.Api;
 using DailyRutine.Application;
 using DailyRutine.Application.Options;
 using DailyRutine.Persistance;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,6 +24,24 @@ builder.Services.AddControllers();
 
 builder.Services.Configure<JwtOptions>(builder.Configuration.GetSection("JwtOptions"));
 
+
+builder.Services.AddAuthentication(options => {
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options => {
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ClockSkew = TimeSpan.Zero,
+        ValidIssuer = builder.Configuration["JwtOptions:Issuer"],
+        ValidAudience = builder.Configuration["JwtOptions:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JwtOptions:Key"]))
+    };
+});
+
 var app = builder.Build();
 
 app.UseGlobalExceptionMiddleware();
@@ -32,6 +54,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapControllers();
 
